c6720a3a074e94b587d5dcd1420441fe006e2799
Aritroo Kumar Chowdhury <aritroo123chowdhury52@gmail.com>
2023-01-26 13:26:04 +0530

    ++

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Flutter App',
+      title: 'App',
       home: MyHomePage(),
     );
   }
