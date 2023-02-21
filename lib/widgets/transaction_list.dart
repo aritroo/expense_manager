5fd3709ed6501679e202f1fd444fbd9fcebea1fb
Aritroo Kumar Chowdhury <aritroo123chowdhury52@gmail.com>
2023-02-21 22:18:58 +0530

    ++

---
+++ lib/widgets/transaction_list.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import 'package:flutter/material.dart';
+
+class TransactionList extends StatefulWidget {
+  const TransactionList({super.key});
+
+  @override
+  State<TransactionList> createState() => _TransactionListState();
+}
+
+class _TransactionListState extends State<TransactionList> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
