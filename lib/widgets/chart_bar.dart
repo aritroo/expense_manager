a92eaff9d39a22514891141aaea597d79285dc9f
Aritroo Kumar Chowdhury <aritroo123chowdhury52@gmail.com>
2023-02-25 05:16:46 +0530

    ++

--- lib/widgets/chart_bar.dart
+++ lib/widgets/chart_bar.dart
@@ -24,7 +24,7 @@ class ChartBar extends StatelessWidget {
                 decoration: BoxDecoration(
                   border: Border.all(color: Colors.grey, width: 1.0),
                   color: Color.fromRGBO(220, 220, 220, 1),
-                  borderRadius: BorderRadius.circular(10),
+                  borderRadius: BorderRadius.circular(2),
                 ),
               ),
               FractionallySizedBox(
@@ -32,7 +32,7 @@ class ChartBar extends StatelessWidget {
                 child: Container(
                   decoration: BoxDecoration(
                     color: Theme.of(context).colorScheme.primary,
-                    borderRadius: BorderRadius.circular(10),
+                    borderRadius: BorderRadius.circular(2),
                   ),
                 ),
               ),
